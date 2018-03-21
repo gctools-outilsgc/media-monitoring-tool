26d2eb3870179d7e015591dac25f50ee559d3a87
HoussamBedja <houssam.b@hotmail.com>
2018-03-21 11:31:06 -0400

    Add Group Script: exeptions fixed

--- GCMonitor/src/AddGroupScript.groovy
+++ GCMonitor/src/AddGroupScript.groovy
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.util.ArrayList
+
 import groovy.json.JsonSlurper
 
 /*--------------------------------------SCRIPT-----------------------------------------------*/
@@ -93,6 +95,107 @@ public UserInfo getUserInfo() {
 	return user
 }
 
+
+
+//get Forums from a specific Group
+public ArrayList<Forum> getForums(Group g) {
+	def list = new ArrayList<Forum>()
+	def cmd = new ArrayList<String>()
+
+	def parser = new JsonSlurper()
+	def id
+	Forum f
+
+	def post
+	def postRC
+	def responseString
+	def response
+	def url
+
+	cmd.add("discussion")
+	cmd.add("blog")
+	cmd.add("event")
+
+	id = g.getID()
+	
+	for(String s in cmd) {
+				
+		try {
+		url = new URL("https://gccollab.ca/services/api/rest/json/?method=query.posts&user=" + userInfo.getUser() + "&password=" + userInfo.getPassword() + "&object=" + s + "&group=" + id)
+		post = url.openConnection()
+		post.requestMethod = 'POST'
+		post.setDoOutput(true)
+		postRC = post.getResponseCode()
+		} catch(java.net.ConnectException e) {
+			post = url.openConnection()
+			post.requestMethod = 'POST'
+			post.setDoOutput(true)
+			postRC = post.getResponseCode()
+			continue
+		}
+		
+		//FOR TESTING
+		println("--------------------BREAKER LINE-----------------")
+		println("This is id: " + id)
+		println("This is cmd: " + s)
+		
+		if(postRC == 200) {
+
+			responseString = post.getInputStream().getText()
+			response = parser.parseText(responseString)
+
+			if(s.equals("discussion")) {
+				for(def i = 0; i<response.result.size();i++) {
+					f = new Discussion(response.result.get(i).guid, g, new URL(response.result.get(i).url), response.result.get(i).description, response.result.get(i).title,getTimestamp(response.result.get(i).time_updated.toString()))
+
+					if(getTimestamp(response.result.get(i).time_created) > timestamp) {
+						f.notifyNew()
+					} else if(getTimestamp(response.result.get(i).time_updated) > timestamp) {
+						f.notifyOfChange()
+					}
+
+					getMessages(f,response.result.get(i).replies)
+					list.add(f)
+				}
+			}
+
+			if(s.equals("blog")) {
+				for(def i = 0; i<response.result.size();i++) {
+					f = new Blog(response.result.get(i).guid, g, new URL(response.result.get(i).url), response.result.get(i).description, response.result.get(i).title,getTimestamp(response.result.get(i).time_updated.toString()))
+
+					if(getTimestamp(response.result.get(i).time_created) > timestamp) {
+						f.notifyNew()
+					} else if(getTimestamp(response.result.get(i).time_updated) > timestamp) {
+						f.notifyOfChange()
+					}
+
+					getMessages(f,response.result.get(i).replies)
+					list.add(f)
+				}
+			}
+
+			if(s.equals("event")) {
+				for(def i = 0; i<response.result.size();i++) {
+					f = new Event(response.result.get(i).guid, g, new URL(response.result.get(i).url), response.result.get(i).description, response.result.get(i).title,getTimestamp(response.result.get(i).time_updated.toString()))
+
+					if(getTimestamp(response.result.get(i).time_created) > timestamp) {
+						f.notifyNew()
+					} else if(getTimestamp(response.result.get(i).time_updated) > timestamp) {
+						f.notifyOfChange()
+					}
+
+					getMessages(f,response.result.get(i).replies)
+					list.add(f)
+				}
+			}
+		} else {
+			println("This is error code: " + postRC)
+		}
+		
+		postRC = null
+	}
+	return list
+}
 
 
 
